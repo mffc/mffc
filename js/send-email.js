6bb4b395cd7868d77edcc0733fafeb70c44e8b95
Arthur Yuen <yarthur@gmail.com>
2014-04-21 10:39:45 +1000

    try email to info

--- js/send-email.js
+++ js/send-email.js
@@ -1,7 +1,17 @@
 
-     $(document).ready(function() {
-$("button#submit").click(function(){
-  alert("ok");    
-});
+$("#contact_form").submit(function(event){
+  event.preventDefault();
+  var form = $(this);
+  var name = $(this).find("#inputname").val();
+  var phone = $(this).find("#inputphone").val();
+  var email = $(this).find("#inputemail").val();
+  var church = $(this).find("#inputchurch").val();
+  var question = $(this).find("#inputquestion").val();
+  
+  $.post("process.php", {name: name, phone: phone, email: email, church: church, question: question})
+    .done( function(data) {
+      alert ("done");
+    });
+
 });
 
